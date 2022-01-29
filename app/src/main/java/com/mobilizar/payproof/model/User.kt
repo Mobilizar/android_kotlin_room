c42649d95259131207e6a499c133e68b1e4dec6e
Rayckson Araujo <raycksonaraujo@gmail.com>
2022-01-29 13:18:47 -0300

    Arquivo model User excluido, não faz parte do escopo do projeto

--- app/src/main/java/com/mobilizar/payproof/model/User.kt
+++
@@ -1,9 +0,0 @@
-package com.mobilizar.payproof.model
-
-class User(
-    val id: Int,
-    val displayName: String,
-    val email: String,
-    val idToken: String,
-    val profilePicture: String
-)
